62c9ec57bac617932997cd687281e0ed9e56a829
Robin Meis <robin.meis@smartnoob.de>
2015-09-13 17:17:35 +0200

    Frequenzberechnung durch Preprocessor

--- source_code/avr/main.c
+++ source_code/avr/main.c
@@ -1,7 +1,12 @@
 #define F_CPU 1000000
+#define TON_FREQUENZ 250 //Ton Frequenz in Hz
+
+#define CTC_VALUE (F_CPU / 8 / 2 / TON_FREQUENZ)
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+
+volatile unsigned char an=1;
 
 int main(void) {
   DDRB |= (1<<0); //Lautsprecher
@@ -9,16 +14,18 @@ int main(void) {
 
   TCCR0A = (1<<WGM01); //Timer für Schwingkreis
   TCCR0B |= (1<<CS01);
-  OCR0A = 250; //TODO: Frequenz berechnen
+  OCR0A = CTC_VALUE;
   TIMSK |= (1<<OCIE0A);
   sei();
 
   for (;;) {
-    
+
   }
 }
 
 ISR (TIMER0_COMPA_vect) { //Togglen der Lautsprecher Pins bei Interrupt
-  PORTB ^= (1<<4);
-  PORTB ^= (1<<0);
+  if (an) {
+    PORTB ^= (1<<4);
+    PORTB ^= (1<<0);
+  }
 }
