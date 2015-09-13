38430adb30ef29c850203f5f58391a26239b510c
Robin Meis <robin.meis@smartnoob.de>
2015-09-13 21:26:00 +0200

    Programm fertiggestellt

--- source_code/avr/main.c
+++ source_code/avr/main.c
@@ -5,12 +5,21 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
 volatile unsigned char an=1;
 
 int main(void) {
   DDRB |= (1<<0); //Lautsprecher
   DDRB |= (1<<4);
+  unsigned char antischall_an=0;
+
+  DDRB |= (1<<2); //LED
+  unsigned char blinken=0;
+
+  DDRB &= ~(1<<1); //Taster
+  PORTB |=(1<<1);
+  unsigned char zeit_gedrueckt;
 
   TCCR0A = (1<<WGM01); //Timer für Schwingkreis
   TCCR0B |= (1<<CS01);
@@ -19,7 +28,33 @@ int main(void) {
   sei();
 
   for (;;) {
+    if (!(PINB & (1<<1))) {
+      _delay_ms(100);
+      for (zeit_gedrueckt=0;!(PINB & (1<<1)) && zeit_gedrueckt<250; ++zeit_gedrueckt)
+        _delay_ms(2);
 
+      if (zeit_gedrueckt == 250)
+        if (an) an=0;
+        else an=1;
+      else {
+        PORTB ^= (1<<4);
+        if (antischall_an) antischall_an=0;
+        else antischall_an=1;
+        if (!an) an=1;
+      }
+
+      for (;!(PINB & (1<<1)););
+      _delay_ms(100);
+    }
+
+    if (!an) {
+      if (blinken == 250) { blinken=0; PORTB ^= (1<<2); }
+      else ++blinken;
+      _delay_ms(2);
+    } else if (antischall_an) {
+      PORTB |= (1<<2);
+    } else
+      PORTB &= ~(1<<2);
   }
 }
 
